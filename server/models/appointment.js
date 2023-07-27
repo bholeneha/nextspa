5276e42638ad44b411c96550b45c2df52cf13b25
Neha Bhole <nehasbhole@gmail.com>
2023-07-28 01:58:42 +0530

    add appointment model schema

---
+++ server/models/appointment.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+// Define Appointment Model
+const appointmentSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  with: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+});
+
+// Create Appointment Model
+const Appointment = mongoose.model("Appointment", appointmentSchema);
+
+module.exports = Appointment;
