2802e431b53b422cf19f6ac4100c063717a81753
Neha Bhole <nehasbhole@gmail.com>
2023-07-28 01:52:32 +0530

    add user model schema

---
+++ server/models/users.js
@@ -0,0 +1,30 @@
+// server/models/userModel.js
+const mongoose = require("mongoose");
+
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  appointments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Appointment",
+    },
+  ],
+});
+
+// Create the User model
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
