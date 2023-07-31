89950aba1bbc68ad902c29170d2c0e763358bfb4
Neha Bhole <nehasbhole@gmail.com>
2023-08-01 01:17:13 +0530

    add book meeting button and position it bottom right

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect } from 'react';
 import HomeLayout from '../components/templates/home/home';
+import Button from '../components/atoms/button/button';
 import { useRouter, usePathname } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import './dashboard.scss';
@@ -20,7 +21,9 @@ const Dashboard: React.FC = () => {
     <HomeLayout>
       <div className='dashboard' > 
         <h1>WELCOME {data?.user?.name?.toUpperCase()}</h1>
-        <div className='upcoming-meetings'></div>
+        <div className='upcoming-meetings'>
+          <Button children='BOOK NEW MEETING' />
+        </div>
         <div className='past-meetings'>
           <h2>Past Meetings</h2>
         </div>
