1b521f902275b15144ea81c522fe6e8b1e1e8898
Neha Bhole <nehasbhole@gmail.com>
2023-07-31 16:42:33 +0530

    other config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXTAUTH_SECRET: "your-secret-goes-here", // Replace with your actual secret
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
