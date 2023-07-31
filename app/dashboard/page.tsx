9b1dc530c5f6560de57f4f5e3d8d58c5bef61d65
Neha Bhole <nehasbhole@gmail.com>
2023-07-31 16:40:18 +0530

    dashboard to display logged in user name

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import HomeLayout from '../components/templates/home/home';
+'use client';
 
-interface DashboardData {
-}
+import React, { useEffect } from 'react';
+import HomeLayout from '../components/templates/home/home';
+import { useRouter, usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 const Dashboard: React.FC = () => {
 
+  const router = useRouter();
+
+  const {data, status: sessionStatus} = useSession()
+
+  useEffect(() => {
+    if(sessionStatus === 'unauthenticated') {
+      router.push('/login');
+    }
+  }, [sessionStatus, router])
+
   return (
     <HomeLayout>
-      <h1>Welcome User</h1>
+      {data?.user ? (
+        <h1>Welcome {data.user.name}</h1>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </HomeLayout>
   );
 };
