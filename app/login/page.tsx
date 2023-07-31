e52fa176eed1f2b5f65eb7b6b82f0903deea8ccf
Neha Bhole <nehasbhole@gmail.com>
2023-07-31 16:41:54 +0530

    redirect to dashboard if login successful

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,27 +1,43 @@
 'use client';
 
-import React from "react";
+import React, {useState} from 'react';
 import HomeLayout from "../components/templates/home/home";
 import LoginForm from '../components/organisms/loginform/loginform';
 import { useRouter } from 'next/navigation';
+import { signIn } from 'next-auth/react';
+import './loginpage.scss';
 
 interface LoginFormProps {
   onSubmit: () => void;
+  hasError: boolean;
+}
+
+interface LoginValues {
+  email: string;
+  password: string;
 }
 
 const LoginPage: React.FC = () => {
+  const [hasError, setHasError] = useState<boolean>(false);
   const router = useRouter();
 
-  const handleSubmit = () => {
+  const handleSubmit = async (values: LoginValues) => {
     console.log("Submitting...");
-    router.push('/dashboard');
+    console.log(values);
+    const signInResponse = await signIn('credentials', {email:values.email, password:values.password, redirect: false})
+    console.log(signInResponse)
+    setHasError(!!signInResponse?.error);
+    if (signInResponse?.ok) {
+      router.push('/dashboard');
+    }
   };
 
   return (
     <HomeLayout>
-        <h1>Log In</h1>
-        <p>Please log in to see your dashboard!</p>
-        <LoginForm onSubmit={handleSubmit}/>
+        <div className='login-form'>
+          <h1>Log In</h1>
+            <LoginForm onSubmit={handleSubmit} error={hasError} />
+        </div>
     </HomeLayout>
   );
 };
